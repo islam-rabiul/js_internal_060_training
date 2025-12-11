c20737ba4e60bed03b8863daab229d8d0d5dee87
Mohammad Rabiul Islam <156330532+islam-rabiul@users.noreply.github.com>
2025-12-11 05:31:42 +0000

    doneday4

---
+++ Day4/index.js
@@ -0,0 +1,6 @@
+let mydate=new Date();
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.getDate());
+console.log(mydate.getDay());
+console.log(mydate.getMonth());
